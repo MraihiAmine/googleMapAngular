e26f300609d43a4be13cc71f2f55e0760dcc4a77
Mraihi Amine <mraihiamin@gmail.com>
2022-09-10 07:35:41 +0100

    errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,75 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { chartModal, HighchartService } from './highchart.service';
-import * as Highcharts from "highcharts";
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = "Firestore-Angular-Highcharts";
+  zoom = 12;
+  maxZoomTemp = 15;
+  minZoomTemp = 8;
+  center!: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: this.maxZoomTemp,
+    minZoom: this.minZoomTemp,
+  };
+  title = 'Firestore-Angular-Highcharts';
   items$!: chartModal[];
   Highcharts: typeof Highcharts = Highcharts;
   chardata: any[] = [];
   chartOptions: any;
+
   constructor(private highchartservice: HighchartService) {}
+
   ngOnInit() {
+    
     this.highchartservice.rates$.subscribe((assets) => {
       this.items$ = assets;
       if (this.items$) {
+        this.chardata = [];
         this.items$.forEach((element) => {
+          console.log(this.chardata);
           this.chardata.push(element.rate);
         });
         this.getChart();
       }
     });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });    
   }
   getChart() {
     this.chartOptions = {
-      series: [{
-        data: this.chardata,
-      }, ],
+      series: [
+        {
+          data: this.chardata,
+        },
+      ],
       chart: {
-        type: "bar",
+        type: 'bar',
       },
       title: {
-        text: "barchart",
+        text: 'barchart',
       },
     };
+  }
+  zoomIn() {
+    if (this.zoom < this.minZoomTemp) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.maxZoomTemp) this.zoom--;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,15 @@ import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 import { HighchartsChartModule } from 'highcharts-angular';
-
+import { GoogleMapsModule } from '@angular/google-maps'
 @NgModule({
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     AngularFirestoreModule,
-    HighchartsChartModule
+    HighchartsChartModule,
+    GoogleMapsModule
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
